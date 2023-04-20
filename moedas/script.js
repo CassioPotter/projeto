21c5d40cde70d62a38170d5a808f821206b02c5e
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-04-19 21:16:29 -0300

    Create script.js

---
+++ moedas/script.js
@@ -0,0 +1,10 @@
+
+function btnVal() {
+    
+    var outDol = document.getElementById("outDol")
+    var outLi = document.getElementById("outLi")
+    var outEu = document.getElementById("outEu")
+    var outBit = document.getElementById("outBit")
+
+    outDol.innerHTML = "R$ 4,99"
+}
