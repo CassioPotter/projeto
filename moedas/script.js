50ff71c035dda6fda7d741eb7f109e7aaa8f694b
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-04-20 23:37:21 -0300

    Update script.js

--- moedas/script.js
+++ moedas/script.js
@@ -1,14 +1,27 @@
+// https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL
+
+
+    
 
 function btnVal() {
     
     var outDol = document.getElementById("outDol")
-    var outLi = document.getElementById("outLi")
     var outEu = document.getElementById("outEu")
     var outBit = document.getElementById("outBit")
 
-    outDol.innerHTML = "R$ 4,99"
-}
+    const url = ' https://economia.awesomeapi.com.br/last/'
+    const moedas = 'USD-BRL,EUR-BRL,BTC-BRL'
 
-function btnEnter() {
+fetch(url + moedas)
+    .then(function (response) {
+        return response.json()
     
+    })
+    .then(function (data) {
+        console.log(data)
+      
+    })
+    outDol.innerHTML = USDBRL.bid
+    outEu.innerHTML = `R$ 5,78 `
+    outBit.innerHTML = `R$ 29.484,68`
 }
